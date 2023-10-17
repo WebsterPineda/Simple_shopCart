244a15a285e8791d29776ae4639c92cb27dd455b
Webster Pineda <38181952+WebsterPineda@users.noreply.github.com>
2023-10-16 22:27:00 -0600

    Added dependencies for Spring application

--- simple-shop-api/build.gradle.kts
+++ simple-shop-api/build.gradle.kts
@@ -1,20 +1,41 @@
 plugins {
     java
-
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
 }
 
 group = "com.webster"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.4")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.1.4")
+    implementation("org.flywaydb:flyway-core:9.22.3")
+
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.2.0")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
+
 tasks.getByName<Test>("test") {
+    systemProperty("file.encoding", "UTF-8")
     useJUnitPlatform()
 }
