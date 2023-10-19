f606ffeb37277a53b260b5b814f5c65e99b2695e
Webster Pineda <38181952+WebsterPineda@users.noreply.github.com>
2023-10-18 19:01:53 -0600

    Append ComponentScan annotation

--- simple-shop-api/src/main/java/com/webster/Main.java
+++ simple-shop-api/src/main/java/com/webster/Main.java
@@ -2,8 +2,10 @@ package com.webster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.webster")
 public class Main {
 
     public static void main(String[] args) {
