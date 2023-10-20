04359ed48e92f47308a09e76d725ef05bd54dc8c
Webster Pineda <38181952+WebsterPineda@users.noreply.github.com>
2023-10-20 02:36:23 -0600

    Modifying Controllers to use their respective services

--- simple-shop-api/src/main/java/com/webster/controller/ClienteController.java
+++ simple-shop-api/src/main/java/com/webster/controller/ClienteController.java
@@ -1,29 +1,61 @@
 package com.webster.controller;
 
-import com.webster.models.Cliente;
-import com.webster.repository.ClienteRepository;
+import com.webster.dto.ClienteDTO;
+import com.webster.response.BaseResponse;
+import com.webster.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping(value = "/api/cliente", produces = "application/json")
 public class ClienteController {
 
     @Autowired
-    private ClienteRepository clienteRepository;
+    private ClientService service;
 
     @GetMapping()
-    public List<Cliente> getClientes(){
-        return clienteRepository.findAll();
+    public BaseResponse<List<ClienteDTO>> getClientes() {
+        return service.getAllClients();
     }
 
     @GetMapping("/{id}")
-    public Cliente getCliente(@PathVariable Long id) {
-        return clienteRepository.findById(id).orElse(null);
+    public BaseResponse<ClienteDTO> getCliente(@PathVariable Long id) {
+        return service.getClienteById(id);
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<BaseResponse<ClienteDTO>> createClient(@RequestBody ClienteDTO client) {
+        BaseResponse<ClienteDTO> created = service.createCliente(client);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getData().getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PutMapping(value = "/{id}", consumes = "application/json")
+    public BaseResponse<ClienteDTO> updateClient(@PathVariable Long id, @RequestBody ClienteDTO client) {
+        return service.updateCliente(id, client);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
+        service.deleteCliente(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- simple-shop-api/src/main/java/com/webster/controller/OrdenCompraEController.java
+++ simple-shop-api/src/main/java/com/webster/controller/OrdenCompraEController.java
@@ -1,28 +1,95 @@
 package com.webster.controller;
 
-import com.webster.models.OrdenCompraE;
-import com.webster.repository.OrdenCompraERepository;
+import com.webster.dto.SellDTO;
+import com.webster.dto.SellDetailDTO;
+import com.webster.response.BaseResponse;
+import com.webster.services.OrdenCompraDService;
+import com.webster.services.OrdenCompraEService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/venta")
+@RequestMapping(value = "/api/venta", produces = "application/json")
 public class OrdenCompraEController {
     @Autowired
-    private OrdenCompraERepository ordenCompraERepository;
+    private OrdenCompraEService service;
+
+    @Autowired
+    private OrdenCompraDService detailService;
 
     @GetMapping
-    public List<OrdenCompraE> getVentas(){
-        return ordenCompraERepository.findAll();
+    public BaseResponse<List<SellDTO>> getVentas() {
+        return service.getAllVentas();
     }
 
     @GetMapping("/{id}")
-    public OrdenCompraE getDetalleVenta(@PathVariable Long id){
-        return ordenCompraERepository.findById(id).orElse(new OrdenCompraE());
+    public BaseResponse<SellDTO> getVenta(@PathVariable Long id) {
+        return service.getVenta(id);
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<BaseResponse<SellDTO>> createOrdenCompra(@RequestBody SellDTO sell) {
+        BaseResponse<SellDTO> created = service.createSell(sell);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getData().getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PutMapping(value = "/{id}", consumes = "application/json")
+    public BaseResponse<SellDTO> updateSell(@PathVariable Long id, @RequestBody SellDTO sell) {
+        return service.updateSell(id, sell);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteSell(@PathVariable Long id) {
+        service.deleteSell(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // Endpoints for details
+    @GetMapping("/{sellId}/detail")
+    public BaseResponse<List<SellDetailDTO>> getDetallesVentaPorVentaId(@PathVariable Long sellId) {
+        return detailService.getAllDetallesVenta(sellId);
+    }
+
+    @PostMapping(value = "/{sellId}/detail", consumes = "application/json")
+    public ResponseEntity<BaseResponse<SellDetailDTO>> creadeDetalleVenta(@PathVariable Long sellId,
+                                                                          @RequestBody SellDetailDTO sellDetail) {
+        BaseResponse<SellDetailDTO> created = detailService.createSellDetail(sellId, sellDetail);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{sellId}/detail/{detailId}")
+                .buildAndExpand(sellId,
+                        created.getData().getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PutMapping("/{sellId}/detail/{detailId}")
+    public BaseResponse<SellDetailDTO> updateDetailSell(@PathVariable Long sellId, @PathVariable Long detailId, @RequestBody SellDetailDTO detail) {
+        return detailService.updateSellDetail(sellId, detailId, detail);
+    }
+
+    @DeleteMapping("/{sellId}/detail/{detailId}")
+    public ResponseEntity<Void> deleteDetail(@PathVariable Long sellId, @PathVariable Long detailId) {
+        detailService.deleteSellDetail(sellId, detailId);
+        return ResponseEntity.noContent().build();
     }
 }
