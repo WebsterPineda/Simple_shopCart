8c1a6ac9e870d3b5da86a023d2ab135444a91f33
Webster Pineda <38181952+WebsterPineda@users.noreply.github.com>
2023-10-19 20:17:26 -0600

    Constants for responses code

---
+++ simple-shop-api/src/main/java/com/webster/enums/Responses.java
@@ -0,0 +1,21 @@
+package com.webster.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Responses {
+
+    SUCCESS(0, "Process end with success"),
+    NOT_FOUND(404, "Resource not found"),
+    GURU_BUSSY(999, "We're really sorry! sometimes things go wrong, " +
+            "it seems that one of our systems has failed and the guru is at mediation. " +
+            "Please, try again in few minutes, maybe we already restore service");
+
+    private final int code;
+    private final String description;
+
+    Responses(int code, String description){
+        this.code = code;
+        this.description = description;
+    }
+}
