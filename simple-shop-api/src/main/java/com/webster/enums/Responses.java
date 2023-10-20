9ce765b21b4264c6104364968a8aa06d3a3e430e
Webster Pineda <38181952+WebsterPineda@users.noreply.github.com>
2023-10-20 02:34:12 -0600

    Corrected a typo

--- simple-shop-api/src/main/java/com/webster/enums/Responses.java
+++ simple-shop-api/src/main/java/com/webster/enums/Responses.java
@@ -7,14 +7,14 @@ public enum Responses {
 
     SUCCESS(0, "Process end with success"),
     NOT_FOUND(404, "Resource not found"),
-    GURU_BUSSY(999, "We're really sorry! sometimes things go wrong, " +
+    GURU_BUSY(999, "We're really sorry! sometimes things go wrong, " +
             "it seems that one of our systems has failed and the guru is at mediation. " +
             "Please, try again in few minutes, maybe we already restore service");
 
     private final int code;
     private final String description;
 
-    Responses(int code, String description){
+    Responses(int code, String description) {
         this.code = code;
         this.description = description;
     }
